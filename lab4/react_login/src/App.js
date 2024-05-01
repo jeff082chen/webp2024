69e984ed764595bb770d4eb9952eea6f822fc06a
Jeffrey Chen <jeff082chen@gmail.com>
2024-05-01 15:23:20 +0800

    lab4

---
+++ lab4/react_login/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SignInSide from './SignInSide';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <SignInSide />
+      </div>
+    </div>
+  );
+}
+
+export default App;
